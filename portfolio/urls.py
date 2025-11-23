c0e4c49e49cc93142ffc99037d3dc736e0321054
uttamkl <royu86831@gmail.com>
2025-11-23 17:11:01 +0530

    Initial commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView   # <-- IMPORTANT
+from django.views.static import serve   # <-- Add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Google verification file
-    path('google6df42595b7db69b3.html',
-         TemplateView.as_view(template_name='google6df42595b7db69b3.html')),
+    path('google6df42595b7db69b3.html', serve, {'path': 'google6df42595b7db69b3.html', 'document_root': settings.STATIC_ROOT}),
 
     # Core app
     path('', include('core.urls')),
