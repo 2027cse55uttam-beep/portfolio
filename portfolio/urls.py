e19312dbf709286cbfd5dffb41d356c6786a7e66
uttamkl <royu86831@gmail.com>
2025-11-23 17:03:31 +0530

    added google verification file

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,13 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView   # <-- IMPORTANT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # <-- YE MAIN HAI: Core app connect kiya
+
+    # Google verification file
+    path('google6df42595b7db69b3.html',
+         TemplateView.as_view(template_name='google6df42595b7db69b3.html')),
+
+    # Core app
+    path('', include('core.urls')),
 ]
 
-# Debug mode mein static/media serve karne ke liye (development ke liye)
+# DEBUG mode for static/media
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
